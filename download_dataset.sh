7dcb2e4a735052a761a39ea15b55f362e307939e
Andrew Han <handrew11@gmail.com>
2018-05-11 11:13:59 -0700

    modified file structure

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,14 +1,16 @@
-echo "==========  Downloading all datasets."
-python data/download_coco.py
+# echo "==========  Downloading all datasets."
+# python data/download_coco.py
 
-echo "==========  Removing zip files."
-rm *.zip
+# echo "==========  Removing zip files."
+# rm *.zip
 
 echo "==========  Downloading train / val captions"
 curl -O http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 unzip annotations_trainval2014.zip -d .
 mv annotations/captions_train2014.json data/train
 mv annotations/captions_val2014.json data/val
+mv annotations/instances_train2014.json data/train
+mv annotations/instances_val2014.json data/val
 rm -rf annotations/
 
 echo "==========  Downloading test captions"
