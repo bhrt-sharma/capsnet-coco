ad4ad3919753a6d17c99fb355dd4b16a179059ae
Andrew Han <handrew11@gmail.com>
2018-05-12 14:38:34 -0700

    Revert "remove the stupid relative import in the cocoapi. hopefully this doesn't break anything"
    This reverts commit 9bd8e1b5325cd9c7b364e5d4f9ecf60bac9852b6.

--- data/cocoapi/pycoco/pycocotools/coco.py
+++ data/cocoapi/pycoco/pycocotools/coco.py
@@ -54,8 +54,8 @@ from matplotlib.patches import Polygon
 import numpy as np
 import copy
 import itertools
-import mask as maskUtils
-import _mask
+from . import mask as maskUtils
+import pycocotools._mask as _mask
 import os
 from collections import defaultdict
 import sys
