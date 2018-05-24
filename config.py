ff38769e38e605d59d6bff9215134bb0aecf9cd4
Andrew Han <handrew11@gmail.com>
2018-05-24 11:49:11 -0700

    try train cnn baseline on remote

--- config.py
+++ config.py
@@ -34,8 +34,8 @@ flags.DEFINE_string('dataset', 'data/mnist', 'the path for dataset')
 flags.DEFINE_string('dataset_fashion_mnist', 'data/fashion_mnist', 'the path for dataset')
 flags.DEFINE_boolean('is_train', True, 'train or predict phase')
 flags.DEFINE_integer('num_threads', 8, 'number of threads of enqueueing exampls')
-flags.DEFINE_string('logdir', 'logdir', 'logs directory')
-flags.DEFINE_string('test_logdir', 'test_logdir', 'test logs directory')
+flags.DEFINE_string('logdir', 'logs', 'logs directory')
+flags.DEFINE_string('test_logdir', 'test_logs', 'test logs directory')
 
 cfg = tf.app.flags.FLAGS
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -8,55 +8,6 @@ from keras.datasets import cifar10, cifar100
 from keras import backend as K
 
 
-def create_inputs_mnist(is_train):
-    tr_x, tr_y = load_mnist(cfg.dataset, is_train)
-    data_queue = tf.train.slice_input_producer([tr_x, tr_y], capacity=64 * 8)
-    x, y = tf.train.shuffle_batch(data_queue, num_threads=8, batch_size=cfg.batch_size, capacity=cfg.batch_size * 64,
-                                  min_after_dequeue=cfg.batch_size * 32, allow_smaller_final_batch=False)
-
-    return (x, y)
-
-
-def create_inputs_fashion_mnist(is_train):
-    tr_x, tr_y = load_mnist(cfg.dataset_fashion_mnist, is_train)
-    data_queue = tf.train.slice_input_producer([tr_x, tr_y], capacity=64 * 8)
-    x, y = tf.train.shuffle_batch(data_queue, num_threads=8, batch_size=cfg.batch_size, capacity=cfg.batch_size * 64,
-                                  min_after_dequeue=cfg.batch_size * 32, allow_smaller_final_batch=False)
-
-    return (x, y)
-
-
-def load_mnist(path, is_training):
-    fd = open(os.path.join(cfg.dataset, 'train-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    trX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float32)
-
-    fd = open(os.path.join(cfg.dataset, 'train-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    trY = loaded[8:].reshape((60000)).astype(np.int32)
-
-    fd = open(os.path.join(cfg.dataset, 't10k-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float32)
-
-    fd = open(os.path.join(cfg.dataset, 't10k-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    teY = loaded[8:].reshape((10000)).astype(np.int32)
-
-    # normalization and convert to a tensor [60000, 28, 28, 1]
-    # trX = tf.convert_to_tensor(trX, tf.float32)
-    # teX = tf.convert_to_tensor(teX, tf.float32)
-
-    # => [num_samples, 10]
-    # trY = tf.one_hot(trY, depth=10, axis=1, dtype=tf.float32)
-    # teY = tf.one_hot(teY, depth=10, axis=1, dtype=tf.float32)
-
-    if is_training:
-        return trX, trY
-    else:
-        return teX, teY
-
-
 def create_inputs_cifar10(is_train):
     tr_x, tr_y = load_cifar10(is_train)
     data_queue = tf.train.slice_input_producer([tr_x, tr_y], capacity=64 * 8)
@@ -64,7 +15,6 @@ def create_inputs_cifar10(is_train):
                                   min_after_dequeue=cfg.batch_size * 32, allow_smaller_final_batch=False)
 
     return (x, y)
-
 
 def load_cifar10(is_training):
     # https://keras.io/datasets/
@@ -115,9 +65,8 @@ def load_cifar100(is_training):
     else:
         return cifar100.load_data(label_mode='fine')[1]
 
-def get_create_inputs(dataset_name: str, is_train: bool, epochs: int):
-    options = {'mnist': lambda: create_inputs_mnist(is_train),
-               'fashion_mnist': lambda: create_inputs_mnist(is_train),
-               'cifar10': lambda: create_inputs_cifar10(is_train),
-               'cifar100': lambda: create_inputs_cifa100(is_train)}
+def get_create_inputs(dataset_name: str, is_train: bool, epochs: int, config):
+    options = {'cifar10': lambda: create_inputs_cifar10(is_train, config),
+               'cifar100': lambda: create_inputs_cifar100(is_train, config),
+               'mscoco': lambda: create_inputs_mscoco(is_train, config)}
     return options[dataset_name]
