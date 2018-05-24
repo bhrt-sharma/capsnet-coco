912d9a5b4e86f4757ef9ab4069c648450a947711
Andrew Han <handrew11@gmail.com>
2018-05-24 11:09:00 -0700

    merge conflicts

--- train_cnn_baseline.py
+++ train_cnn_baseline.py
@@ -1,52 +1,39 @@
 """
 License: Apache-2.0
 Author: Suofei Zhang | Hang Yu
-E-mail: zhangsuofei at njupt.edu.cn | hangyu5 at illinois.edu
+
+Modified by Andrew Han
 """
 
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-from config import cfg, get_coord_add, get_dataset_size_train, get_num_classes, get_create_inputs
+from config import cfg, get_dataset_size_train, get_num_classes, get_create_inputs
 import time
 import numpy as np
 import sys
 import os
-import capsnet_em as net
-
-import logging
-import daiquiri
-
-daiquiri.setup(level=logging.DEBUG)
-logger = daiquiri.getLogger(__name__)
+from models.cnn_baseline import build_cnn_baseline
 
 
-def main(args):
-    """Get dataset hyperparameters."""
-    assert len(args) == 2 and isinstance(args[1], str)
-    dataset_name = args[1]
-    logger.info('Using dataset: {}'.format(dataset_name))
-
-    """Set reproduciable random seed"""
+def main():
+    """Set reproducible random seed"""
     tf.set_random_seed(1234)
 
-    coord_add = get_coord_add(dataset_name)
     dataset_size = get_dataset_size_train(dataset_name)
     num_classes = get_num_classes(dataset_name)
     create_inputs = get_create_inputs(dataset_name, cfg, is_train=True, epochs=cfg.epoch)
 
     with tf.Graph().as_default(), tf.device('/cpu:0'):
         """Get global_step."""
-        global_step = tf.get_variable(
-            'global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+        global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
 
         """Get batches per epoch."""
         num_batches_per_epoch = int(dataset_size / cfg.batch_size)
 
         """Use exponential decay leanring rate?"""
-        lrn_rate = tf.maximum(tf.train.exponential_decay(
-            1e-3, global_step, num_batches_per_epoch, 0.8), 1e-5)
+        lrn_rate = tf.maximum(tf.train.exponential_decay(1e-3, global_step, num_batches_per_epoch, 0.8), 1e-5)
         tf.summary.scalar('learning_rate', lrn_rate)
-        opt = tf.train.AdamOptimizer()  # lrn_rate
+        opt = tf.train.AdamOptimizer() 
 
         """Get batch from data queue."""
         batch_x, batch_labels = create_inputs()
@@ -57,7 +44,7 @@ def main(args):
             with slim.arg_scope([slim.variable], device='/cpu:0'):
                 batch_x_squash = tf.divide(batch_x, 255.)
                 batch_x = slim.batch_norm(batch_x, center=False, is_training=True, trainable=True)
-                output = net.build_arch_baseline(batch_x, is_train=True,
+                output = net.build_cnn_baseline(batch_x, is_train=True,
                                                  num_classes=num_classes)
                 loss, recon_loss, _ = net.cross_ent_loss(output, batch_x_squash, batch_labels)
                 acc = net.test_accuracy(output, batch_labels)
@@ -84,8 +71,7 @@ def main(args):
         sess.run(tf.global_variables_initializer())
 
         """Set Saver."""
-        var_to_save = [v for v in tf.global_variables(
-        ) if 'Adam' not in v.name]  # Don't save redundant Adam beta/gamma
+        var_to_save = [v for v in tf.global_variables() if 'Adam' not in v.name]  # Don't save redundant Adam beta/gamma
         saver = tf.train.Saver(var_list=var_to_save, max_to_keep=cfg.epoch)
 
         """Display parameters"""
