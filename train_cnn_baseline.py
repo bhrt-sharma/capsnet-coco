0c842609400fbf35fc020b8b644348b3f2f7d958
Andrew Han <handrew11@gmail.com>
2018-05-25 12:07:03 -0700

    really starting to eviscerate the cnn baseline stuff

--- train_cnn_baseline.py
+++ train_cnn_baseline.py
@@ -1,126 +1,75 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from config import cfg
-from utils import create_inputs_mscoco
+from utils import create_inputs_mscoco, load_mscoco
 import time
 import numpy as np
 import sys
 import os
+from tqdm import tqdm
 from models.cnn_baseline import build_cnn_baseline
 
 
 def main():
-    """Set reproducible random seed"""
     tf.set_random_seed(1234)
 
-    with tf.Graph().as_default(), tf.device('/cpu:0'):
-        """Get global_step."""
-        global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+    """ GET DATA """
+    dataset = load_mscoco('train', cfg, return_dataset=True)
+    N, D = dataset.X.shape[0], dataset.X.shape[1]
+    num_classes = max(dataset.y)
+    num_batches_per_epoch = int(N / cfg.batch_size)
 
-        """Get batches per epoch."""
-        num_batches_per_epoch = int(dataset_size / cfg.batch_size)
+    """ SET UP INITIAL VARIABLES"""
+    initial_learning_rate = 1e-3 # maybe move this to config?
+    lrn_rate = tf.maximum(tf.train.exponential_decay(initial_learning_rate, global_step, num_batches_per_epoch, 0.8), 1e-5)
+    tf.summary.scalar('learning_rate', lrn_rate)
+    opt = tf.train.AdamOptimizer(lrn_rate) 
+    global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
 
-        """Use exponential decay learning rate?"""
-        lrn_rate = tf.maximum(tf.train.exponential_decay(1e-3, global_step, num_batches_per_epoch, 0.8), 1e-5)
-        tf.summary.scalar('learning_rate', lrn_rate)
-        opt = tf.train.AdamOptimizer(lrn_rate) 
+    """ DEFINE DATA FLOW """
+    batch_x = tf.placeholder(tf.float32, shape=(cfg.batch_size, D, D, 3))
+    batch_x_squash = tf.divide(batch_x, 255.)
+    batch_x = slim.batch_norm(batch_x, center=False, is_training=True, trainable=True)
+    output = build_cnn_baseline(batch_x, is_train=True, num_classes=num_classes)
+    loss, recon_loss, _ = cross_ent_loss(output, batch_x_squash, batch_labels)
+    acc = test_accuracy(output, batch_labels)
+    
+    tf.summary.scalar('train_acc', acc)
+    tf.summary.scalar('recon_loss', recon_loss)
+    tf.summary.scalar('all_loss', loss)
 
-        """Get batch from data queue."""
-        batch_x, batch_labels = create_inputs_mscoco(is_training, cfg)
-        # batch_y = tf.one_hot(batch_labels, depth=10, axis=1, dtype=tf.float32)
+    """Compute gradient."""
+    grad = opt.compute_gradients(loss)
+    grad_check = [tf.check_numerics(g, message='Gradient NaN Found!')
+                  for g, _ in grad if g is not None] + [tf.check_numerics(loss, message='Loss NaN Found')]
 
-        dataset_size = batch_x.shape[0]
-        num_classes = max(batch_labels)
+    """Apply gradient."""
+    with tf.control_dependencies(grad_check):
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_op = opt.apply_gradients(grad, global_step=global_step)
 
-        """Define the dataflow graph."""
-        with tf.device('/gpu:0'):
-            with slim.arg_scope([slim.variable], device='/cpu:0'):
-                batch_x_squash = tf.divide(batch_x, 255.)
-                batch_x = slim.batch_norm(batch_x, center=False, is_training=True, trainable=True)
-                output = net.build_cnn_baseline(batch_x, is_train=True,
-                                                 num_classes=num_classes)
-                loss, recon_loss, _ = net.cross_ent_loss(output, batch_x_squash, batch_labels)
-                acc = net.test_accuracy(output, batch_labels)
-                tf.summary.scalar('train_acc', acc)
-                tf.summary.scalar('recon_loss', recon_loss)
-                tf.summary.scalar('all_loss', loss)
+    """ RUN GRAPH """
+    with tf.Session() as sess:
+        if cfg.phase == 'train':
+            sess.run(tf.local_variables_initializer())
+            sess.run(tf.global_variables_initializer())
+            tf.get_default_graph().finalize()
 
-            """Compute gradient."""
-            grad = opt.compute_gradients(loss)
-            # See: https://stackoverflow.com/questions/40701712/how-to-check-nan-in-gradients-in-tensorflow-when-updating
-            grad_check = [tf.check_numerics(g, message='Gradient NaN Found!')
-                          for g, _ in grad if g is not None] + [tf.check_numerics(loss, message='Loss NaN Found')]
+            """Set summary writer"""
+            if not os.path.exists(cfg.logdir + '/cnn_baseline/{}/train_log/'.format(dataset_name)):
+                os.makedirs(cfg.logdir + '/cnn_baseline/{}/train_log/'.format(dataset_name))
+            summary_writer = tf.summary.FileWriter(
+                cfg.logdir + '/cnn_baseline/{}/train_log/'.format(dataset_name), graph=sess.graph)
 
-        """Apply graident."""
-        with tf.control_dependencies(grad_check):
-            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-            with tf.control_dependencies(update_ops):
-                train_op = opt.apply_gradients(grad, global_step=global_step)
-
-        """Set Session settings."""
-        sess = tf.Session(config=tf.ConfigProto(
-            allow_soft_placement=True, log_device_placement=False))
-        sess.run(tf.local_variables_initializer())
-        sess.run(tf.global_variables_initializer())
-
-        """Set Saver."""
-        var_to_save = [v for v in tf.global_variables() if 'Adam' not in v.name]  # Don't save redundant Adam beta/gamma
-        saver = tf.train.Saver(var_list=var_to_save, max_to_keep=cfg.epoch)
-
-        """Display parameters"""
-        total_p = np.sum([np.prod(v.get_shape().as_list()) for v in var_to_save]).astype(np.int32)
-        train_p = np.sum([np.prod(v.get_shape().as_list())
-                          for v in tf.trainable_variables()]).astype(np.int32)
-        logger.info('Total Parameters: {}'.format(total_p))
-        logger.info('Trainable Parameters: {}'.format(train_p))
-
-        # read snapshot
-        # latest = os.path.join(cfg.logdir, 'model.ckpt-4680')
-        # saver.restore(sess, latest)
-        """Set summary op."""
-        summary_op = tf.summary.merge_all()
-
-        """Start coord & queue."""
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-
-        """Set summary writer"""
-        if not os.path.exists(cfg.logdir + '/cnn_baseline/{}/train_log/'.format(dataset_name)):
-            os.makedirs(cfg.logdir + '/cnn_baseline/{}/train_log/'.format(dataset_name))
-        summary_writer = tf.summary.FileWriter(
-            cfg.logdir + '/cnn_baseline/{}/train_log/'.format(dataset_name), graph=sess.graph)
-
-        """Main loop."""
-        for step in range(cfg.epoch * num_batches_per_epoch + 1):
-            tic = time.time()
-            """"TF queue would pop batch until no file"""
-            try:
-                _, loss_value, summary_str = sess.run(
-                    [train_op, loss, summary_op])
-                logger.info('%d iteration finishs in ' % step + '%f second' %
-                            (time.time() - tic) + ' loss=%f' % loss_value)
-            except KeyboardInterrupt:
-                sess.close()
-                sys.exit()
-            except tf.errors.InvalidArgumentError:
-                logger.warning('%d iteration contains NaN gradients. Discard.' % step)
-                continue
-            else:
-                """Write to summary."""
-                if step % 5 == 0:
-                    summary_writer.add_summary(summary_str, step)
-
-                """Epoch wise linear annealling."""
-                if (step % num_batches_per_epoch) == 0:
-
-                    """Save model periodically"""
-                    ckpt_path = os.path.join(
-                        cfg.logdir + '/cnn_baseline/{}'.format(dataset_name), 'model-{:.4f}.ckpt'.format(loss_value))
-                    saver.save(sess, ckpt_path, global_step=step)
-
-        """Join threads"""
-        coord.join(threads)
-
+            """Main loop"""
+            for _ in tqdm(list(range(cfg.num_epochs)), desc='epoch'):
+                for _ in tqdm(list(range(dataset.num_batches)), desc='batch'):
+                    batch = dataset.next_batch()
+                    feed_dict = {images: batch}
+                    _, loss_value, summary_str = sess.run([train_op, loss, summary_op], feed_dict=feed_dict)
+                    train_writer.add_summary(summary, global_step)
+                dataset.reset()
 
 if __name__ == "__main__":
     tf.app.run()
--- utils/__init__.py
+++ utils/__init__.py
@@ -7,6 +7,39 @@ from config import cfg
 from keras.datasets import cifar10, cifar100
 from keras import backend as K
 
+def load_mscoco(dataset_type, config, return_dataset=False):
+    if dataset_type == 'train':
+        if config.use_masked:
+            data = Dataset("data/train/images/masked")
+        else:
+            data = Dataset("data/train/images/train2014")
+    elif dataset_type == 'test':
+        if config.use_masked:
+            data = Dataset("data/test/images/masked")
+        else:
+            data = Dataset("data/test/images/test2014")
+    elif dataset_type == 'val':
+        if config.use_masked:
+            data = Dataset("data/val/images/masked")
+        else:
+            data = Dataset("data/val/images/val2014")
+    else:
+        raise ValueError("Dataset type must be one of 'train', 'test', or 'val'")
+
+    if return_dataset:
+        return data
+
+    return (data.X, data.y)
+
+ 
+# FUNCTIONS FROM OLD REPO
+
+def create_inputs_mscoco(is_training, config):
+    tr_x, tr_y = load_mscoco(is_training, config)
+    data_queue = tf.train.slice_input_producer([tr_x, tr_y], capacity=64 * 8)
+    x, y = tf.train.shuffle_batch(data_queue, num_threads=8, batch_size=config.batch_size, capacity=config.batch_size * 64,
+                                  min_after_dequeue=config.batch_size * 32, allow_smaller_final_batch=False)
+    return (x, y)
 
 def create_inputs_cifar10(is_train):
     tr_x, tr_y = load_cifar10(is_train)
@@ -23,27 +56,6 @@ def load_cifar10(is_training):
         return cifar10.load_data()[0]
     else:
         return cifar10.load_data()[1]
-
-def create_inputs_mscoco(is_training, config):
-    tr_x, tr_y = load_mscoco(is_training, config)
-    data_queue = tf.train.slice_input_producer([tr_x, tr_y], capacity=64 * 8)
-    x, y = tf.train.shuffle_batch(data_queue, num_threads=8, batch_size=config.batch_size, capacity=config.batch_size * 64,
-                                  min_after_dequeue=config.batch_size * 32, allow_smaller_final_batch=False)
-    return (x, y)
-
-def load_mscoco(is_training, config):
-    if is_training:
-        if config.use_masked:
-            data = Dataset("data/train/images/masked")
-        else:
-            data = Dataset("data/train/images/train2014")
-    else:
-        if config.use_masked:
-            data = Dataset("data/test/images/masked")
-        else:
-            data = Dataset("data/test/images/train2014")
-
-    return (data.X, data.y)
 
 def create_inputs_cifar100(is_train):
     tr_x, tr_y = load_cifar100(is_train)
