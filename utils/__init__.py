5698a2d7e3b23b0076824473b5c86038b1fea7c3
handrew <handrew@stanford.edu>
2018-05-27 02:58:07 +0000

    new variable to loading mscoco

--- utils/__init__.py
+++ utils/__init__.py
@@ -8,22 +8,22 @@ from keras.datasets import cifar10, cifar100
 from keras import backend as K
 from .dataset import Dataset
 
-def load_mscoco(dataset_type, config, return_dataset=False):
+def load_mscoco(dataset_type, config, num=None, return_dataset=False):
     if dataset_type == 'train':
         if config.use_masked:
-            data = Dataset("data/train/images/masked", is_train=True, batch_size=config.batch_size, num=1000)
+            data = Dataset("data/train/images/masked", is_train=True, batch_size=config.batch_size, num=num)
         else:
-            data = Dataset("data/train/images/train2014", batch_size=config.batch_size, is_train=True)
+            data = Dataset("data/train/images/train2014", batch_size=config.batch_size, num=num, is_train=True)
     elif dataset_type == 'test':
         if config.use_masked:
-            data = Dataset("data/test/images/masked", batch_size=config.batch_size)
+            data = Dataset("data/test/images/masked", batch_size=config.batch_size, num=num)
         else:
-            data = Dataset("data/test/images/test2014", batch_size=config.batch_size)
+            data = Dataset("data/test/images/test2014", batch_size=config.batch_size, num=num)
     elif dataset_type == 'val':
         if config.use_masked:
-            data = Dataset("data/val/images/masked", batch_size=config.batch_size)
+            data = Dataset("data/val/images/masked", batch_size=config.batch_size, num=num)
         else:
-            data = Dataset("data/val/images/val2014", batch_size=config.batch_size)
+            data = Dataset("data/val/images/val2014", batch_size=config.batch_size, num=num)
     else:
         raise ValueError("Dataset type must be one of 'train', 'test', or 'val'")
 
