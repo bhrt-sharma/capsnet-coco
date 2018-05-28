fd297b3e277f24e69905c571a9d4ca630686a38d
handrew <handrew@stanford.edu>
2018-05-28 07:06:43 +0000

    get rid of some errors

--- scripts/create_train_val_test_sets.py
+++ scripts/create_train_val_test_sets.py
@@ -10,74 +10,72 @@ TEST_FOLDER = 'data/test/images/final/'
 
 
 def copy_files_over():
-	pass
+    pass
 
-def get_train_test_val():
-	train_files = os.listdir(TRAIN_FOLDER)
-	val_files = os.listdir(VAL_FOLDER)
-	test_files = os.listdir(TEST_FOLDER)
+def get_train_test_val_info():
+    train_files = os.listdir(TRAIN_FOLDER)
+    val_files = os.listdir(VAL_FOLDER)
+    test_files = os.listdir(TEST_FOLDER)
 
-	train_files = [f for f in train_files if '.jpg' in f]
-	val_files = [f for f in val_files if '.jpg' in f]
-	test_files = [f for f in test_files if '.jpg' in f]
+    train_files = [f for f in train_files if '.jpg' in f]
+    val_files = [f for f in val_files if '.jpg' in f]
+    test_files = [f for f in test_files if '.jpg' in f]
 
-	import json
-	print("Dumping.")
-	file_names = {"train": train_files, "val": val_files, "test": test_files}
-	with open("train_test_val_split.json", "w+") as f:
-		json.dump(file_names, f)
+    import json
+    print("Dumping.")
+    file_names = {"train": train_files, "val": val_files, "test": test_files}
+    with open("train_test_val_split.json", "w+") as f:
+        json.dump(file_names, f)
 
 
 def shuffle():
-	num_train = 80000
-	num_val = 40000
-	num_test = 40000
+    num_train = 80000
+    num_val = 40000
+    num_test = 40000
 
-	# get files and counts of categories
-	masked_files = os.listdir(ORIGIN_FOLDER)
-	masked_files = [f for f in masked_files if '.jpg' in f]
+    # get files and counts of categories
+    masked_files = os.listdir(ORIGIN_FOLDER)
+    masked_files = [f for f in masked_files if '.jpg' in f]
 
-	print("There are {} masked files.".format(len(masked_files)))
+    print("There are {} masked files.".format(len(masked_files)))
 
-	category_dict = {}
-	for f in tqdm(masked_files, desc="loading to dict"):
-		cat_id = f.split("_")[-2]
-		img = f
-		category_dict[cat_id] = category_dict.get(cat_id, []) + [img]
+    category_dict = {}
+    for f in tqdm(masked_files, desc="loading to dict"):
+        cat_id = f.split("_")[-2]
+        img = f
+        category_dict[cat_id] = category_dict.get(cat_id, []) + [img]
 
-	num_categories = len(category_dict.keys())
+    num_categories = len(category_dict.keys())
 
-	print("There are {} categories.".format(num_categories))
+    print("There are {} categories.".format(num_categories))
 
-	# choose files for train test and val 
-	for category in tqdm(category_dict, desc="category"):
-		images = np.random.shuffle(category_dict[category])
+    # choose files for train test and val 
+    for category in tqdm(category_dict, desc="category"):
+        images = category_dict[category]
+        np.random.shuffle(images)
 
-		num_train_files_to_get = num_train / num_categories
-		num_val_files_to_get = num_val / num_categories
-		num_test_files_to_get = num_test / num_categories
+        num_train_files_to_get = num_train // num_categories
+        num_val_files_to_get = num_val // num_categories
+        num_test_files_to_get = num_test // num_categories
 
-		assert num_train_files_to_get + num_val_files_to_get + num_test_files_to_get < len(images)
+        assert num_train_files_to_get + num_val_files_to_get + num_test_files_to_get < len(images)
 
-		print("Getting train files for category {}.".format(category))
-		for tr in tqdm(range(num_train_files_to_get)):
-			f_name = images[tr]
-			os.system("cp {}/{} {}".format(ORIGIN_FOLDER, f_name, TRAIN_FOLDER))
-			images.remove(f_name)
+        print("Getting {} train files for category {}.".format(num_train_files_to_get, category))
+        for tr in range(num_train_files_to_get):
+            f_name = images.pop(0)
+            os.system("cp {}/{} {}".format(ORIGIN_FOLDER, f_name, TRAIN_FOLDER))
 
-		print("Getting val files for category {}.".format(category))
-		for v in tqdm(range(num_val_files_to_get)):
-			f_name = images[v]
-			os.system("cp {}/{} {}".format(ORIGIN_FOLDER, f_name, VAL_FOLDER))
-			images.remove(f_name)
+        print("Getting {} val files for category {}.".format(num_val_files_to_get, category))
+        for v in range(num_val_files_to_get):
+            f_name = images.pop(0)
+            os.system("cp {}/{} {}".format(ORIGIN_FOLDER, f_name, VAL_FOLDER))
 
-		print("Getting test files for category {}.".format(category))
-		for te in tqdm(range(num_test_files_to_get)):
-			f_name = images[te]
-			os.system("cp {}/{} {}".format(ORIGIN_FOLDER, f_name, TEST_FOLDER))
-			images.remove(f_name)
+        print("Getting {} test files for category {}.".format(num_test_files_to_get, category))
+        for te in range(num_test_files_to_get):
+            f_name = images.pop(0)
+            os.system("cp {}/{} {}".format(ORIGIN_FOLDER, f_name, TEST_FOLDER))
 
 
 if __name__ == '__main__':
-	shuffle()
-	get_train_test_val()
+    shuffle()
+    get_train_test_val_info()
