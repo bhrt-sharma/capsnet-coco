a79accd090461a723a5a80b5a1c8c1d62c0c6b2b
Andrew Han <handrew11@gmail.com>
2018-05-28 17:41:46 -0700

    one hot encode the label vector

--- train_capsnet.py
+++ train_capsnet.py
@@ -21,6 +21,8 @@ def main(_):
       global_step = tf.train.get_or_create_global_step()
 
     images, labels = train_dataset.X.astype(np.float32), train_dataset.y
+    labels = np.zeros((len(labels), 91 + 1))[np.arange(len(labels)), labels] = 1 # one hot encode that shit 
+
     poses, activations = nets.capsules_v0(images, num_classes=91, iterations=1, name='capsulesEM-V0')
 
     # margin schedule
