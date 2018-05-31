caa9c49a81a458448f0b2087ab6262487862f4fd
handrew <handrew@stanford.edu>
2018-05-31 03:29:11 +0000

    change learning rate to be slower so it doens't blow out histogram

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ flags.DEFINE_float('ac_lambda0', 0.01, '\lambda in the activation function a_c, 
 flags.DEFINE_float('ac_lambda_step', 0.01,
                    'It is described that \lambda increases at each iteration with a fixed schedule, however specific super parameters is absent.')
 
-flags.DEFINE_float('initial_learning_rate', 1e-2, '\lambda in the activation function a_c, iteration 0')
+flags.DEFINE_float('initial_learning_rate', 1e-3, '\lambda in the activation function a_c, iteration 0')
 
 flags.DEFINE_integer('batch_size', 25, 'batch size')
 flags.DEFINE_integer('num_epochs', 50, 'number of epochs')
