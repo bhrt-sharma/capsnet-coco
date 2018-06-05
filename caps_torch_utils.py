66d8574a83ee3f664829f6234b1c065f4e4319c6
Andrew Han <handrew11@gmail.com>
2018-06-05 16:25:12 -0700

    try pytorch

--- caps_torch_utils.py
+++ caps_torch_utils.py
@@ -8,6 +8,7 @@ Created on Sat Nov 18 12:25:51 2017
 import argparse
 import torch
 from torchvision import datasets, transforms
+from utils import load_tless_split
 
 #TODO: data augmentation
 #def augmentation(x, max_shift=2):
@@ -24,46 +25,20 @@ from torchvision import datasets, transforms
 #                 target_height_slice, target_width_slice]
 #    return shifted_image.float()
 
-def get_dataloader(args):
+def get_dataloader(cfg):
     # MNIST Dataset
-    train_dataset = datasets.MNIST(root='./data/',
-                                   train=True,
-                                   transform=transforms.ToTensor(),
-                                   download=True)
-
-    test_dataset = datasets.MNIST(root='./data/',
-                                  train=False,
-                                  transform=transforms.ToTensor())
+    train_dataset, val, test_dataset = load_tless_split(cfg, num_classes=2)
 
     # Data Loader (Input Pipeline)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                               batch_size=args.batch_size,
+                                               batch_size=cfg.batch_size,
                                                shuffle=True)
 
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                              batch_size=args.batch_size,
+                                              batch_size=cfg.batch_size,
                                               shuffle=True)
 
     return train_loader, test_loader
-
-
-def get_args():
-    parser = argparse.ArgumentParser(description='CapsNet')
-
-    parser.add_argument('-batch_size', type=int, default=128)
-    parser.add_argument('-num_epochs', type=int, default=1)
-    parser.add_argument('-lr', type=float, default=2e-2)
-    parser.add_argument('-clip', type=float, default=5)
-    parser.add_argument('-r', type=int, default=3)
-    parser.add_argument('-disable_cuda', action='store_true',
-                    help='Disable CUDA')
-    parser.add_argument('-print_freq', type=int, default=10)
-    parser.add_argument('-pretrained', type=str, default="")
-    parser.add_argument('-gpu', type=int, default=0, help = "which gpu to use") 
-    args = parser.parse_args()
-    args.use_cuda = not args.disable_cuda and torch.cuda.is_available()
-
-    return args
 
 
 if __name__ == "__main__":
